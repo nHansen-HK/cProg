f439fa9f4da4d265017881a5c9650c55551ff036
nHansen-HK <127867389+nHansen-HK@users.noreply.github.com>
2024-04-29 07:00:55 -0400

    Add files via upload
    upload

--- oppgave3.c
+++ oppgave3.c
@@ -189,26 +189,110 @@ void findTableN()
 }
 TableNode* getResByTblNum(int tblNum)
 {
+    // Point the search pointer to the first table 
     TableNode* current = firstTable;
 
-    return current;
-    
+    while (current != NULL) { // search unitl end of list
+        // if you find correct value, exit before end
+        if (current->tableNumber == tblNum) break;
+        current = current->next;
+    }
+    return current; // returns NULL if nothing found (reached end), and correct reservation if foudn
 }
-//  FUNCTIONS FOR ADDING ORDERS
-void addOrder()
+
+TableNode* getResByName(char* name) // same as function above 
 {
+    TableNode* current = firstTable;
+
+    while (current != NULL) {
+        // use case-insesetive vversion to make search less painful
+        if (strcasecmp(current->customer, name) == 0) break;
+        current = current->next;
+    }
+    return current;
+}
+
+//  FUNCTIONS FOR ADDING ORDERS
+int insertOrder(TableNode* reservation, char* purchaser, char* ordername, int price)
+{
+    // searching pointer for orders
+    OrderNode* current = NULL;
+    // allocate for order
+    OrderNode* newOrder = malloc(sizeof(OrderNode));
+    if (newOrder == NULL) {
+        printf("Allocation error for order\n");
+        return 0;
+    }
+    // allocate the string and copy from the addOrder buffer
+    newOrder->purchaser = malloc(strlen(purchaser) + 1);
+    if (newOrder->purchaser != NULL) {
+        strcpy(newOrder->purchaser, purchaser);
+    } else {
+        printf("String allocation error, purchaser name\n");
+        return 0;
+    }
+    // also allocate for the ordername
+    newOrder->orderName = malloc(strlen(ordername) + 1);
+    if (newOrder->orderName != NULL) {
+        strcpy(newOrder->orderName, ordername);
+    } else {
+        printf("String allocation error, orderName\n");
+        return 0;
+    }
+    // set price
+    newOrder->price = price;
+
+    // INSERTION INTO LIST
+    current = reservation->firstOrder; // set search pointer to the first Order
+    newOrder->next = NULL; // This must be done no matter the state of the list
+    // if no orders for reservation
+    if (current == NULL) {
+        reservation->firstOrder = newOrder;
+    } else {
+        // go to the last order in list
+        while (current->next != NULL) {
+            current = current->next; 
+        }
+        // then insert at end
+        current->next = newOrder;
+    }
+    // order inserted successfully
+    return 1;
+}
+int addOrder()
+{
+    int success;
+    // data
+    TableNode* reservation = NULL;
     int tblNum;
-    printf("Table:\n");
-    scanf("%d", &tblNum);
-
-    printf("What is your name\n");
-
-    /*
-    void* next;
-    char* purchaser;
-    char* orderName;
+    char nameBuf[16];
+    char foodBuf[21];
     int price;
-    */
+
+    printf("Table:\n");
+    success = scanf("%d", &tblNum);
+    if (success != 1) return 0;
+
+    printf("What is the first name, max 15 characters\n");
+    success = scanf("%s15", nameBuf);
+    if (success != 1) return 0;
+
+    printf("Name of dish/drink: \n");
+    success = scanf("%s20", foodBuf);
+    if (success != 1) return 0;
+
+    printf("Price of dish/drink: \n");
+    success = scanf("%d", &price);
+    if (success != 1) return 0;
+
+    reservation = getResByTblNum(tblNum);
+
+    if (insertOrder(reservation, nameBuf, foodBuf, price) != 1) {
+        printf("Order insert error\n");
+        return 0;
+    }
+
+    return 1;
 }
 
 //  MENU AND MAIN FUNCTIONALITY 
@@ -258,11 +342,39 @@ int main()
                 case 2: // get N table
                     findTableN();
                     break;
-                case 3: // get reservation by name and print info 
+                case 3: {
+                    TableNode* reservation = NULL;
+                    char name[21];
+                    //clear the input buffer again
+                    int c;
+                    while((c = getchar()) != '\n' && c != EOF);
+                    // get name with potetial spaces
+                    printf("Name to search for: \n");
+                    if (fgets(name, sizeof(name), stdin) != NULL) {
+                        name[strcspn(name, "\n")] = 0;
+                    } else {
+                        printf("Error in getting name\n");
+                        break;
+                    } 
+                    
+                    reservation = getResByName(name);
+                    if (reservation == NULL) {
+                        printf("No reservation found under that name");
+                        break;
+                    }
+                    printTableInfo(reservation);
                     break;
+                }
+                    // get reservation by name and print info 
                 case 4: // delete a reservation
                     break;
                 case 5: // add an order
+                    //test
+                    if (addOrder() == 1) {
+                        printf("Order Added Successfully\n");
+                    } else {
+                        printf("Something went wrong with the order\n");
+                    }
                     break;
                 case 6: // print all orders of given table
                     break;
